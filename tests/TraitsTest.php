56b2e579a928340742a33f8ce978bd790f6ef5c9
Leonardo Marquine <leonardomarquine@gmail.com>
2017-09-23 23:12:06 -0300

    Remove indexable trait

--- tests/TraitsTest.php
+++ tests/TraitsTest.php
@@ -3,30 +3,11 @@
 namespace Tests;
 
 use Tests\TestCase;
-use Marquine\Etl\Traits\Indexable;
 use Marquine\Etl\Traits\ValidateSource;
 
 class TraitsTest extends TestCase
 {
-    use Indexable, ValidateSource;
-
-    /** @test */
-    function indexable()
-    {
-        $items = [
-            ['id' => '1', 'name' => 'John Doe', 'email' => 'johndoe@email.com'],
-            ['id' => '2', 'name' => 'Jane Doe', 'email' => 'janedoe@email.com'],
-        ];
-
-        $users = $this->index($items, ['id', 'name']);
-
-        $expected = [
-            '1John Doe' => ['id' => '1', 'name' => 'John Doe', 'email' => 'johndoe@email.com'],
-            '2Jane Doe' => ['id' => '2', 'name' => 'Jane Doe', 'email' => 'janedoe@email.com'],
-        ];
-
-        $this->assertEquals($expected, $users);
-    }
+    use ValidateSource;
 
     /** @test */
     function validate_source()

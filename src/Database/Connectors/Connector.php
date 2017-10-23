fb1959227ee9f22fd12de13ff581f0095a781ef7
Leonardo Marquine <leonardomarquine@gmail.com>
2017-10-23 16:17:41 -0200

    Fix pdo options merge

--- src/Database/Connectors/Connector.php
+++ src/Database/Connectors/Connector.php
@@ -6,25 +6,24 @@ use PDO;
 
 abstract class Connector
 {
-
     /**
      * The default PDO connection options.
      *
      * @var array
      */
     protected $options = [
-        PDO::ATTR_CASE               => PDO::CASE_NATURAL,
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_ORACLE_NULLS       => PDO::NULL_NATURAL,
-        PDO::ATTR_STRINGIFY_FETCHES  => false,
-        PDO::ATTR_EMULATE_PREPARES   => true,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL,
+        PDO::ATTR_STRINGIFY_FETCHES => false,
+        PDO::ATTR_EMULATE_PREPARES => true,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ];
 
     /**
      * Connect to a database.
      *
-     * @param array $config
+     * @param  array  $config
      * @return \PDO
      */
     abstract public function connect($config);
@@ -32,22 +31,29 @@ abstract class Connector
     /**
      * Create a new PDO connection.
      *
-     * @param string $dsn
-     * @param array $config
+     * @param  string  $dsn
+     * @param  array  $config
      * @return \PDO
      */
     public function createConnection($dsn, array $config)
     {
-        // Merge custom PDO options from the connection config if available
-        if (array_key_exists('pdo_options', $config) && is_array($config['pdo_options'])) {
-            $this->options = array_merge($this->options, $config['pdo_options']);
-        }
+        $username = $config['username'] ?? null;
 
-        $username = isset($config['username']) ? $config['username'] : null;
+        $password = $config['password'] ?? null;
 
-        $password = isset($config['password']) ? $config['password'] : null;
-
-        return new PDO($dsn, $username, $password, $this->options);
+        return new PDO($dsn, $username, $password, $this->getOptions($config));
     }
 
+    /**
+     * Get the PDO options based on the configuration.
+     *
+     * @param  array  $config
+     * @return array
+     */
+    public function getOptions(array $config)
+    {
+        $options = $config['options'] ?? [];
+
+        return array_diff_key($this->options, $options) + $options;
+    }
 }

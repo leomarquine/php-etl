88fd3a33a982f7d6ef58f33e77359a99259e8ed5
Leonardo Marquine <leonardomarquine@gmail.com>
2018-08-24 15:27:13 -0300

    Refactor step factory

--- src/Factory.php
+++ src/Factory.php
@@ -8,85 +8,137 @@ use InvalidArgumentException;
 class Factory
 {
     /**
-     * Step instance.
+     * Make a new step.
      *
-     * @var mixed
+     * @param  mixed  $class
+     * @param  string  $interface
+     * @param  array  $options
+     * @return object
      */
-    protected $instance;
-
-    /**
-     * Make a new step and set its options.
-     *
-     * @param  string  $type
-     * @param  string  $step
-     * @param  array|null  $options
-     * @return mixed
-     *
-     * @throws InvalidArgumentException
-     */
-    public function make($type, $step, $options = null)
+    public function make($class, $interface, $options)
     {
-        $this->createStep($type, $step);
-
-        if ($this->instance instanceof $type) {
-            $this->setStepOptions($options);
-
-            return $this->instance;
+        if (is_string($class)) {
+            $class = $this->instance($class, $interface);
         }
 
-        $caller = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2)[1]['function'];
+        $this->setOptions($class, $options);
 
-        throw new InvalidArgumentException("$step is not a valid '$caller' step.");
+        return $this->validate($class, $interface);
     }
 
     /**
-     * Create a new instance of the step.
+     * Create a step instance.
      *
-     * @param  string  $type
-     * @param  string  $step
-     * @return void
+     * @param  string  $class
+     * @param  string  $interface
+     * @return object
+     *
+     * @throws \InvalidArgumentException
      */
-    protected function createStep($type, $step)
+    protected function instance($class, $interface)
     {
-        if (class_exists($step)) {
-            $this->instance = new $step;
-        } elseif (class_exists($step = $this->guessStepClass($type, $step))) {
-            $this->instance = new $step;
+        $class = $this->guessStepClass($class, $interface);
+
+        if (class_exists($class)) {
+            return $this->build($class);
         }
+
+        $step = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 3)[2]['function'];
+
+        throw new InvalidArgumentException("$class is not a valid '$step' step.");
     }
 
     /**
-     * Guess the step class name.
+     * Validate the step against the given interface.
      *
-     * @param  string  $type
+     * @param  mixed  $class
+     * @param  string  $interface
+     * @return object
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function validate($class, $interface)
+    {
+        if ($class instanceof $interface) {
+            return $class;
+        }
+
+        $step = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 3)[2]['function'];
+
+        $class = get_class($class);
+
+        throw new InvalidArgumentException("The class '$class' is not a valid '$step' step.");
+    }
+
+    /**
+     * Recursively build a step and its dependencies.
+     *
+     * @param  string  $class
+     * @return object
+     */
+    protected function build($class)
+    {
+        $reflector = new ReflectionClass($class);
+
+        $constructor = $reflector->getConstructor();
+
+        if (is_null($constructor)) {
+            return new $class;
+        }
+
+        $dependencies = $constructor->getParameters();
+
+        $arguments = [];
+
+        foreach ($dependencies as $dependency) {
+            if ($dependency->getClass()) {
+                $arguments[] = $this->build($dependency->getCLass()->name);
+            }
+        }
+
+        return $reflector->newInstanceArgs($arguments);
+    }
+
+    /**
+     * Guess the step class.
+     *
      * @param  string  $step
+     * @param  string  $interface
      * @return string
      */
-    protected function guessStepClass($type, $step)
+    protected function guessStepClass($step, $interface)
     {
-        $namespace = (new ReflectionClass($type))->getNamespaceName();
+        if (class_exists($step)) {
+            return $step;
+        }
+
+        $namespace = (new ReflectionClass($interface))->getNamespaceName();
 
         $class = implode('', array_map('ucfirst', explode(' ', str_replace(['-', '_'], ' ', $step))));
 
-        return $namespace.'\\'.$class;
+        return $namespace . '\\' . $class;
     }
 
     /**
-     * Set the options of the step.
+     * Set the step options.
      *
+     * @param  object  $class
+     * @param  array  $options
      * @return void
      */
-    protected function setStepOptions($options)
+    protected function setOptions($class, $options)
     {
         if (empty($options)) {
             return;
         }
 
-        $reflector = new ReflectionClass($this->instance);
+        $reflector = new ReflectionClass($class);
 
         foreach ($options as $property => $value) {
+            $property = lcfirst(implode('', array_map('ucfirst', explode(' ', str_replace(['-', '_'], ' ', $property)))));
+
             if ($reflector->hasProperty($property) && $reflector->getProperty($property)->isPublic()) {
-                $this->instance->$property = $value;
+                $class->$property = $value;
             }
         }
     }
--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -8,59 +8,110 @@ use InvalidArgumentException;
 class FactoryTest extends TestCase
 {
     /** @test */
-    public function create_a_new_step_instance_and_set_options()
+    public function creates_a_step_instance_from_a_class_string()
     {
         $factory = new Factory;
 
-        $instance = $factory->make(FakeBaseStep::class, 'FakeStep', ['option' => 'value']);
+        $step = $factory->make(SimpleValidStep::class, StepInterface::class, []);
 
-        $this->assertInstanceOf(FakeStep::class, $instance);
-        $this->assertEquals('value', $instance->option);
+        $this->assertInstanceOf(SimpleValidStep::class, $step);
     }
 
     /** @test */
-    public function normalize_step_name()
+    public function guesses_the_step_class_based_on_the_given_interface()
     {
         $factory = new Factory;
 
-        $this->assertInstanceOf(FakeStep::class, $factory->make(FakeBaseStep::class, 'fakeStep'));
-        $this->assertInstanceOf(FakeStep::class, $factory->make(FakeBaseStep::class, 'fake-step'));
-        $this->assertInstanceOf(FakeStep::class, $factory->make(FakeBaseStep::class, 'fake_step'));
-        $this->assertInstanceOf(FakeStep::class, $factory->make(FakeBaseStep::class, 'fake step'));
+        $this->assertInstanceOf(SimpleValidStep::class, $factory->make('simpleValidStep', StepInterface::class, []));
+        $this->assertInstanceOf(SimpleValidStep::class, $factory->make('simple valid step', StepInterface::class, []));
+        $this->assertInstanceOf(SimpleValidStep::class, $factory->make('simple_valid_step', StepInterface::class, []));
+        $this->assertInstanceOf(SimpleValidStep::class, $factory->make('simple-valid-step', StepInterface::class, []));
     }
 
     /** @test */
-    public function throws_an_exception_for_invalid_step()
+    public function throws_exception_if_the_given_step_does_not_exist()
     {
         $factory = new Factory;
 
         $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage("AnotherFakeStep is not a valid '".__FUNCTION__."' step."); // name from caller's method name
+        $this->expectExceptionMessage("Tests\DoesNotExist is not a valid '".__FUNCTION__."' step.");
 
-        $factory->make(FakeBaseStep::class, 'AnotherFakeStep');
+        $factory->make('does not exist', StepInterface::class, []);
     }
 
     /** @test */
-    public function throws_an_exception_for_inexistent_class()
+    public function recursively_build_step_dependencies()
+    {
+        $factory = new Factory;
+
+        $step = $factory->make('valid step with dependencies', StepInterface::class, []);
+
+        $this->assertInstanceOf(ValidStepWithDependencies::class, $step);
+        $this->assertInstanceOf(Foo::class, $step->foo);
+        $this->assertInstanceOf(Bar::class, $step->foo->bar);
+    }
+
+    /** @test */
+    public function throws_exception_if_step_is_invalid()
     {
         $factory = new Factory;
 
         $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage("InexistentFakeStep is not a valid '".__FUNCTION__."' step."); // name from caller's method name
 
-        $factory->make(FakeBaseStep::class, 'InexistentFakeStep');
+        $factory->make(InvalidStep::class, StepInterface::class, []);
+    }
+
+    /** @test */
+    public function set_provided_step_options()
+    {
+        $factory = new Factory;
+
+        $options = [
+            'configOption1' => 'value1',
+            'config option2' => 'value2',
+            'config_option3' => 'value3',
+            'config-option4' => 'value4',
+        ];
+
+        $step = $factory->make(SimpleValidStep::class, StepInterface::class, $options);
+
+        $this->assertEquals('value1', $step->configOption1);
+        $this->assertEquals('value2', $step->configOption2);
+        $this->assertEquals('value3', $step->configOption3);
+        $this->assertEquals('value4', $step->configOption4);
     }
 }
 
-abstract class FakeBaseStep
+class Bar {}
+
+class Foo
 {
+    public $bar;
+
+    public function __construct(Bar $bar)
+    {
+        $this->bar = $bar;
+    }
 }
 
-class FakeStep extends FakeBaseStep
+interface StepInterface {}
+
+class InvalidStep {}
+
+class SimpleValidStep implements StepInterface
 {
-    public $option;
+    public $configOption1;
+    public $configOption2;
+    public $configOption3;
+    public $configOption4;
 }
 
-class AnotherFakeStep
+class ValidStepWithDependencies implements StepInterface
 {
+    public $foo;
+
+    public function __construct(Foo $foo)
+    {
+        $this->foo = $foo;
+    }
 }

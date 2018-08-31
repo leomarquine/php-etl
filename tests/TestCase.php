e0c0968a31d44b181ece59a9c77ebd4335ccdefc
Leonardo Marquine <leonardomarquine@gmail.com>
2018-08-31 09:40:44 -0300

    Clean up test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,37 +2,9 @@
 
 namespace Tests;
 
-use Marquine\Etl\Etl;
-use Marquine\Etl\Database\Manager as DB;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected function setUp()
-    {
-        parent::setUp();
-
-        Etl::set('path', __DIR__ . '/data');
-
-        Etl::addConnection([
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-        ]);
-
-        Etl::addConnection([
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-        ], 'secondary');
-    }
-
-    protected function createUsersTable($connection, $timestamps = false)
-    {
-        DB::connection($connection)->exec('DROP TABLE IF EXISTS users');
-
-        $statement = $timestamps
-            ? 'CREATE TABLE users (id INTEGER, name VARCHAR(255), email VARCHAR(255), created_at TIMESTAMP, updated_at TIMESTAMP, deleted_at TIMESTAMP)'
-            : 'CREATE TABLE users (id INTEGER, name VARCHAR(255), email VARCHAR(255))';
-
-        DB::connection($connection)->exec($statement);
-    }
+    //
 }

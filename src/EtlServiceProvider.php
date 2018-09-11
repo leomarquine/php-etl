186d25d1e3c8f032af24b68116f3dbce2cc392bb
Leonardo Marquine <leonardomarquine@gmail.com>
2018-09-11 15:01:10 -0300

    Fix Laravel service provider

--- src/EtlServiceProvider.php
+++ src/EtlServiceProvider.php
@@ -7,48 +7,17 @@ use Illuminate\Support\ServiceProvider;
 class EtlServiceProvider extends ServiceProvider
 {
     /**
-     * List of the supported database connections.
-     *
-     * @var array
-     */
-    protected $supportedConnections = [
-        'mysql', 'pgsql', 'sqlite',
-    ];
-
-    /**
      * Perform post-registration booting of services.
      *
      * @return void
      */
     public function boot()
     {
-        Etl::set('path', storage_path('app'));
+        $connections = $this->app['config']['database.connections'];
+        $connections['default'] = $connections[$this->app['config']['database.default']];
 
-        $this->addConnections();
-    }
-
-    /**
-     * Add the connections to the ETL configuration.
-     *
-     * @return void
-     */
-    protected function addConnections()
-    {
-        foreach ($this->getSupportedConnections() as $name => $config) {
-            Etl::addConnection($config, $name == config('database.default') ? 'default' : $name);
+        foreach ($connections as $name => $config) {
+            Etl::service('db')->addConnection($config, $name);
         }
-    }
-
-    /**
-     * Get the supported connections configuration.
-     *
-     * @return array
-     */
-    protected function getSupportedConnections()
-    {
-        return array_intersect_key(
-            config('database.connections'),
-            array_flip($this->supportedConnections)
-        );
     }
 }

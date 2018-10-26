eae2c9568cc25498b0c4facfb35ff85f99735837
Leonardo Marquine <leonardomarquine@gmail.com>
2018-10-26 14:14:54 -0300

    Add row

---
+++ src/Row.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace Marquine\Etl;
+
+use ArrayAccess;
+
+class Row implements ArrayAccess
+{
+    /**
+     * Row attributes.
+     *
+     * @var array
+     */
+    protected $attributes;
+
+    /**
+     * Create a new Row instance.
+     *
+     * @param  array  $attributes
+     */
+    public function __construct(array $attributes)
+    {
+        $this->attributes = $attributes;
+    }
+
+    /**
+     * Set a row attribute
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return void
+     */
+    public function set($key, $value)
+    {
+        $this->attributes[$key] = $value;
+    }
+
+    /**
+     * Get a row attribute.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function get($key)
+    {
+        return $this->attributes[$key] ?? null;
+    }
+
+    /**
+     * Remove a row attribute.
+     *
+     * @param  string  $key
+     * @return void
+     */
+    public function remove($key)
+    {
+        unset($this->attributes[$key]);
+    }
+
+    /**
+     * Transform the given columns using a callback.
+     *
+     * @param  array  $columns
+     * @param  callable  $callback
+     * @return void
+     */
+    public function transform(array $columns, callable $callback)
+    {
+        if (empty($columns)) {
+            $columns = array_keys($this->attributes);
+        }
+
+        foreach ($columns as $column) {
+            $this->$column = call_user_func($callback, $this->$column);
+        }
+    }
+
+    /**
+     * Get the array representation of the row.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * Dynamically retrieve attributes on the row.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        return $this->attributes[$key];
+    }
+
+    /**
+     * Dynamically set attributes on the row.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return void
+     */
+    public function __set($key, $value)
+    {
+        $this->attributes[$key] = $value;
+    }
+
+    /**
+     * Determine if the given attribute exists.
+     *
+     * @param  mixed  $offset
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->attributes[$offset]);
+    }
+
+    /**
+     * Get the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return $this->attributes[$offset];
+    }
+
+    /**
+     * Set the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @param  mixed  $value
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->attributes[$offset] = $value;
+    }
+
+    /**
+     * Unset the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->attributes[$offset]);
+    }
+}
---
+++ tests/RowTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Tests;
+
+use Marquine\Etl\Row;
+
+class RowTest extends TestCase
+{
+    /** @test */
+    public function set_attribute()
+    {
+        $row = new Row([]);
+
+        $row->set('name', 'Jane Doe');
+
+        $this->assertAttributeEquals(['name' => 'Jane Doe'], 'attributes', $row);
+    }
+
+    /** @test */
+    public function get_attribute()
+    {
+        $row = new Row(['name' => 'Jane Doe']);
+
+        $this->assertEquals('Jane Doe', $row->get('name'));
+        $this->assertNull($row->get('invalid'));
+    }
+
+    /** @test */
+    public function remove_attribute()
+    {
+        $row = new Row(['name' => 'Jane Doe']);
+
+        $row->remove('name');
+
+        $this->assertAttributeEquals([], 'attributes', $row);
+    }
+
+    /** @test */
+    public function transform_values_using_a_callback()
+    {
+        $row = new Row(['name' => 'Jane Doe', 'email' => 'janedoe@example.com']);
+
+        $row->transform([], function ($value) {
+            return "*$value*";
+        });
+
+        $this->assertAttributeEquals(['name' => '*Jane Doe*', 'email' => '*janedoe@example.com*'], 'attributes', $row);
+
+        $row->transform(['name'], function ($value) {
+            return trim($value, '*');
+        });
+
+        $this->assertAttributeEquals(['name' => 'Jane Doe', 'email' => '*janedoe@example.com*'], 'attributes', $row);
+    }
+
+    /** @test */
+    public function get_the_array_representation_of_the_row()
+    {
+        $row = new Row(['name' => 'Jane Doe']);
+
+        $this->assertEquals(['name' => 'Jane Doe'], $row->toArray());
+    }
+
+    /** @test */
+    public function array_access()
+    {
+        $row = new Row(['name' => 'Jane Doe']);
+
+        $this->assertTrue(isset($row['name']));
+
+        $this->assertEquals('Jane Doe', $row['name']);
+
+        $row['name'] = 'John Doe';
+
+        $this->assertEquals('John Doe', $row['name']);
+
+        unset($row['name']);
+
+        $this->assertFalse(isset($row['name']));
+    }
+
+    /** @test */
+    public function object_access()
+    {
+        $row = new Row(['name' => 'Jane Doe']);
+
+        $this->assertEquals('Jane Doe', $row->name);
+
+        $row->name = 'John Doe';
+
+        $this->assertEquals('John Doe', $row->name);
+    }
+}

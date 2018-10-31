b124eb29f1b3abfeaea7695372fcb65abce88a75
Leonardo Marquine <leonardomarquine@gmail.com>
2018-10-31 18:25:30 -0300

    Change base loader

--- src/Loaders/Loader.php
+++ src/Loaders/Loader.php
@@ -7,6 +7,26 @@ use Marquine\Etl\Step;
 abstract class Loader extends Step
 {
     /**
+     * The loader output.
+     *
+     * @var mixed
+     */
+    protected $output;
+
+    /**
+     * Set the loader output.
+     *
+     * @param  mixed  $output
+     * @return $this
+     */
+    public function output($output)
+    {
+        $this->output = $output;
+
+        return $this;
+    }
+
+    /**
      * Load the given row.
      *
      * @param  \Marquine\Etl\Row  $row
